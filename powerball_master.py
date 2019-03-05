104458dd87d775a875b4c80d45e784ee38b803d3
tpapka <36523428+tpapka@users.noreply.github.com>
2019-03-05 09:03:17 -0700

    Update powerball_master.py

--- powerball_master.py
+++ powerball_master.py
@@ -7,14 +7,17 @@ fsn = [i for i in range(1, 70)]
 ots = [i for i in range(1, 27)]
 
 def drawing(n=1):
-  for i in range(1, n + 1):
-    print(random.sample(fsn, 5), random.sample(ots, 1))
+    i = 1
+    while i < n + 1:
+         print(random.sample(fsn, 5), random.sample(ots, 1))
+         i += 1
     
 if __name__=="__main__":
-  try:
-    repeat = int(input("How many drawings would you like? "))
-    if repeat > 0:
-      drawing()
-    else:
-      print("You need to provide positive number")
-      
+    try:
+        repeat = int(input("How many drawings would you like? "))
+        if repeat > 0:
+            drawing(repeat)
+        else:
+            print("You need to provide positive number")
+    except:
+        pass
